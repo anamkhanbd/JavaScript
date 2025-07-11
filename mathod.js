d1fc5059fa8380d4df3a5e5a515d0534f8e46c91
Anamul Haque Anam <anamkhanbd2@gmail.com>
2025-07-11 23:55:47 +0600

    updated

---
+++ mathod.js
@@ -0,0 +1,4 @@
+// console.log(Math.round(Math.random()*100)); // prints a random number between 0 and 1
+var a = [1,2,5,74,85,4,75,55,8,4,755,6,55,666,4,55,4,5444,5,4,5,4522,455,8];
+
+console.log(Math.min(...a));
