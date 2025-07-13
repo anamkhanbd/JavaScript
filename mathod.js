8a59bc9db08eac383c7da971c2fd1048ed3e3ed5
Anamul Haque Anam <anamkhanbd2@gmail.com>
2025-07-13 18:42:18 +0600

    refactor: comment out unused console logs and tidy up code

--- mathod.js
+++ mathod.js
@@ -1,4 +1,10 @@
 // console.log(Math.round(Math.random()*100)); // prints a random number between 0 and 1
 var a = [1,2,5,74,85,4,75,55,8,4,755,6,55,666,4,55,4,5444,5,4,5,4522,455,8];
 
-console.log(Math.min(...a));
+// console.log(Math.min(...a));
+ var srt ='A H ANAM KHAN';
+//  console.log(srt.slice(2, 5)); // prints 'H A'
+//  console.log(srt.split(' ')); // splits the string into an array of words
+console.log(srt.replace(/A/g, 'B')); // replaces all occurrences of 'A' with 'B'
+ 
+
