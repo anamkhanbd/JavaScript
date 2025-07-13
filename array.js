2a4673f1ebd2148ced001734571ca6d906037065
Anamul Haque Anam <anamkhanbd2@gmail.com>
2025-07-13 19:24:01 +0600

    refactor: remove commented-out code and clean up array.js

---
+++ array.js
@@ -0,0 +1,28 @@
+let person = ['anam','sami', 'ali', 'sara', 'samir'];
+// console.log(person);// prints the array of names
+
+// printing sami 
+// console.log(typeof person[1]); // prints 'sami'
+
+// loop use to print all names
+
+/*
+for  (let a = 0; a < person.length; a++) {
+    console.log(person[a]); // prints each name in the array
+}
+*/
+/*
+//use forEach to print all names
+person.forEach((name) => {
+    console.log(name); // prints each name in the array
+});
+ */
+
+/*
+//use dowhile loop to print all names
+let i = 0;
+do {
+    console.log(person[i]); // prints each name in the array
+    i++;
+} while (i < person.length);
+*/
