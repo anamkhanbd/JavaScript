3ba439700a908688671ace17a8e87337b8f33eb1
Anamul Haque Anam <anamkhanbd2@gmail.com>
2025-07-15 23:42:02 +0600

    refactor: restore for loop console log and clean up code structure

---
+++ for_vs_while_loop.js
@@ -0,0 +1,16 @@
+/**
+ * print 1 to 10 using a for loop
+ */
+for (let i = 1; i <= 10; i++) {
+     // console.log(i);
+}
+
+/**
+ * print a name with numbering from 1 to 10 using a for loop
+ */
+let a = 1;
+while (a <= 10) {
+     console.log(a);
+     a++;
+}
+
