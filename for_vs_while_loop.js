95974ec79ec2188fdecb84d9ae1d975b97cb02b9
Anamul Haque Anam <anamkhanbd2@gmail.com>
2025-07-16 00:15:31 +0600

    refactor: comment out existing code and add factorial calculation

--- for_vs_while_loop.js
+++ for_vs_while_loop.js
@@ -1,49 +1,49 @@
-const { use } = require("react");
+// const { use } = require("react");
 
-/**
- * print 1 to 10 using a for loop
- */
-for (let i = 1; i <= 10; i++) {
-     // console.log(i);
-}
+// /**
+//  * print 1 to 10 using a for loop
+//  */
+// for (let i = 1; i <= 10; i++) {
+//      // console.log(i);
+// }
 
-/**
- * print a name with numbering from 1 to 10 using a for loop
- */
-let a = 1;
-while (a <= 10) {
-     if (a % 2 === 0) {
-     //     console.log(a);
-     }
-     a++;
-}
+// /**
+//  * print a name with numbering from 1 to 10 using a for loop
+//  */
+// let a = 1;
+// while (a <= 10) {
+//      if (a % 2 === 0) {
+//      //     console.log(a);
+//      }
+//      a++;
+// }
 
-/**
- * print 5 multiples of 10 times using a for loop
- */
-let m = 5;
-for ( let i = 1; i <=10; i++){
-    let result = m * i;
-          // console.log(m+'x'+i+'='+result);
-}
+// /**
+//  * print 5 multiples of 10 times using a for loop
+//  */
+// let m = 5;
+// for ( let i = 1; i <=10; i++){
+//     let result = m * i;
+//           // console.log(m+'x'+i+'='+result);
+// }
 
-/**
- * 1 to 100 numbers summation using a for loop
- */
+// /**
+//  * 1 to 100 numbers summation using a for loop
+//  */
 
-// let number = 100;
-// let sum = number*(1+number)/2;
-// console.log('The sum of 1 to 100 is: ' + sum);
+// // let number = 100;
+// // let sum = number*(1+number)/2;
+// // console.log('The sum of 1 to 100 is: ' + sum);
 
-//try again
-const u = 100;
-const sum = u*(1+u)/2;
-// console.log(sum);
+// //try again
+// const u = 100;
+// const sum = u*(1+u)/2;
+// // console.log(sum);
 
 /**
  * when user type exit close the loop
  */
-let userInput =prompt('Type Anithing to test :');
+// let userInput =prompt('Type Anithing to test :').trim();
 while (userInput !== 'exit') {
     // console.log('You typed: ' + userInput);
     userInput = prompt('Type Anithing to test :');
@@ -52,3 +52,15 @@ while (userInput !== 'exit') {
      alert('Loop exited. You typed exit. Thank you!');
 }
 
+
+
+/**
+ * get user number to fatorual value 
+ */
+
+let userNumber = Number(prompt('Type a number to get its factorial:'));
+let factorial = 1;
+for (let i = 1; i <= userNumber; i++) {
+    factorial *= i;
+}
+console.log('The factorial of ' + userNumber + ' is: ' + factorial);
