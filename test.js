a635ebe12aa62f008a424a52b7dc2e8baee82ab6
Anamul Haque Anam <anamkhanbd2@gmail.com>
2025-07-15 22:47:24 +0600

    refactor: comment out unused loops and add HTML file for script integration

--- test.js
+++ test.js
@@ -1,23 +1,33 @@
-/**
- * make a loop for printing 7 modular under 100
- */
+// /**
+//  * make a loop for printing 7 modular under 100
+//  */
 
-let n = 7;
-for (let i = n; i < 100; i++){
-    if ( i % n == 0) {
-        // console.log(i);
-    }
+// let n = 7;
+// for (let i = n; i < 100; i++){
+//     if ( i % n == 0) {
+//         // console.log(i);
+//     }
+// }
+
+// /**
+//  * event an add loops 
+//  */
+
+// let i = 1 ;
+// for (; i <=20 ; i++){
+//     if (i % 2 == 0){
+//         console.log(i+' ' +"Even");
+//     }else {
+//         console.log(i+' '+"Odd");
+//     }
+// }
+
+let age =Number(prompt("Enter your age: "));
+
+while (age < 18) {
+    console.log("You are not eligible to vote.");
+    age = Number(prompt("Enter your age: "));
 }
-
-/**
- * event an add loops 
- */
-
-let i = 1 ;
-for (; i <=20 ; i++){
-    if (i % 2 == 0){
-        console.log(i+' ' +"Even");
-    }else {
-        console.log(i+' '+"Odd");
-    }
+if (age >= 18) {
+    alert("You are eligible to vote.");
 }
