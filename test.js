8c21c836a55977941bc90e745fa477c4965a6520
Anamul Haque Anam <anamkhanbd2@gmail.com>
2025-07-15 22:23:50 +0600

    refactor: comment out console log for multiples of 7 and adjust loop formatting

--- test.js
+++ test.js
@@ -5,6 +5,19 @@
 let n = 7;
 for (let i = n; i < 100; i++){
     if ( i % n == 0) {
-        console.log(i);
+        // console.log(i);
+    }
+}
+
+/**
+ * event an add loops 
+ */
+
+let i = 1 ;
+for (; i <=20 ; i++){
+    if (i % 2 == 0){
+        console.log(i+' ' +"Even");
+    }else {
+        console.log(i+' '+"Odd");
     }
 }
