bf9412e33289151fc114b620e8c35f3ebe3918af
Anamul Haque Anam <anamkhanbd2@gmail.com>
2025-07-15 18:54:43 +0600

    feat: add switch statement to log the current day of the week

---
+++ swicth.js
@@ -0,0 +1,31 @@
+/**
+ * make a switch statement
+ */
+
+switch (new Date().getDay()) {
+    case 0:
+        console.log("Sunday");
+        break;
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+
+    default:
+        console.log("Have a Error in the switch statement");
+        break;
+}
