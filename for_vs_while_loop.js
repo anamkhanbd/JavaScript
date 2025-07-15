0b9ed744c0191c4d3f7f4530103030fc8b542c61
Anamul Haque Anam <anamkhanbd2@gmail.com>
2025-07-16 00:04:53 +0600

    refactor: uncomment console logs and clean up code structure for clarity

--- for_vs_while_loop.js
+++ for_vs_while_loop.js
@@ -1,3 +1,5 @@
+const { use } = require("react");
+
 /**
  * print 1 to 10 using a for loop
  */
@@ -22,6 +24,31 @@ while (a <= 10) {
 let m = 5;
 for ( let i = 1; i <=10; i++){
     let result = m * i;
-          console.log(m+'x'+i+'='+result);
+          // console.log(m+'x'+i+'='+result);
+}
+
+/**
+ * 1 to 100 numbers summation using a for loop
+ */
+
+// let number = 100;
+// let sum = number*(1+number)/2;
+// console.log('The sum of 1 to 100 is: ' + sum);
+
+//try again
+const u = 100;
+const sum = u*(1+u)/2;
+// console.log(sum);
+
+/**
+ * when user type exit close the loop
+ */
+let userInput =prompt('Type Anithing to test :');
+while (userInput !== 'exit') {
+    // console.log('You typed: ' + userInput);
+    userInput = prompt('Type Anithing to test :');
+}if (userInput === 'exit') {
+    // console.log('Loop exited.');
+     alert('Loop exited. You typed exit. Thank you!');
 }
 
