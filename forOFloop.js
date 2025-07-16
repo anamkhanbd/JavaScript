64f86d174c8de3d705603ed50d3786e08b8ff72c
Anamul Haque Anam <anamkhanbd2@gmail.com>
2025-07-16 23:25:31 +0600

    ForOf loop

---
+++ forOFloop.js
@@ -0,0 +1,15 @@
+// let Names = ['Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Frank', 'Grace', 'Heidi', 'Ivan', 'Judy'];
+
+// for (let name of Names) {
+//     console.log(name);
+// }
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (let num of numbers) {
+    if (num % 2 === 0) {
+        console.log("Event");
+    } else {
+        console.log("odd");
+    }
+}
+// console.log("Loop completed.");
