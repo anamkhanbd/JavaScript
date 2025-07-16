82a0905944108ca3521bf337dc03b94aad69fa45
Anamul Haque Anam <anamkhanbd2@gmail.com>
2025-07-16 22:59:33 +0600

    test with if else

--- forEachloop.js
+++ forEachloop.js
@@ -1,6 +1,22 @@
-let Names =  ['Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Frank', 'Grace', 'Heidi', 'Ivan', 'Judy'];
-Names.forEach(function(anam)
-{
-    console.log(anam);
-});
+// let Names =  ['Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Frank', 'Grace', 'Heidi', 'Ivan', 'Judy'];
+// Names.forEach(function(anam)
+// {
+//     console.log(anam);
+// });
   
+
+
+/**
+ * print 1 to 10 using a forEach loop
+ * showing result in console
+ * event an odd 
+ */
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+numbers.forEach(function(num) {
+    if (num % 2 === 0) {
+        console.log("Event");
+    }else {
+        console.log("odd");
+    }
+});
