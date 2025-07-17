81de4dff3db2a8d5084275a2dbf68884dc460b70
Anamul Haque Anam <anamkhanbd2@gmail.com>
2025-07-17 23:59:46 +0600

    update obj an array

---
+++ object&&array.js
@@ -0,0 +1,24 @@
+/***
+ * make array an make object insite of array
+ */
+
+let Persons = [
+    {
+        name : 'A H ANAM',
+        age  : 25,
+        interest : ['Gaming','Movie','Tour'],
+    },
+    {
+        name : 'Jiahd Sarker',
+        age  : 24,
+        interest : [
+            'Football','Gaming','Eating'
+        ]
+    },{
+        name : 'Mithu Mia',
+        age  : 26,
+        interest : [
+            
+        ]
+    }
+]
