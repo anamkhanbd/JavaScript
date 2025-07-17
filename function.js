1798130daade2838236b997b5931169ff66a7a22
Anamul Haque Anam <anamkhanbd2@gmail.com>
2025-07-17 16:25:45 +0600

    pera mearer function

--- function.js
+++ function.js
@@ -1,13 +1,33 @@
+// /**
+//  * make a function to summation tow numbers
+//  */
+
+// function sum (a,b) {
+//     console.log(a+b);
+// }
+
+// // sum(10,50);
+// let a,b ;
+// a = 80;
+// b = 40;
+// sum(a,b);
+
 /**
- * make a function to summation tow numbers
+ * Make a function an Fizz Buzz Proggram
  */
 
-function sum (a,b) {
-    console.log(a+b);
+function FizzBuzz(){
+        for(let i = 1; i <= 100; i++){
+        if (i % 3 == 0 && i % 5 == 0) {
+            document.write('FizzBuzz<br>');
+        } else if (i % 3 == 0){
+            document.write('Fizz<br>');
+        } else if (i % 5 == 0){
+            document.write('Buzz<br>');
+        } else {
+            document.write(i + '<br>');
+        }
+    }
 }
 
-// sum(10,50);
-let a,b ;
-a = 80;
-b = 40;
-sum(a,b);
+FizzBuzz();
---
+++ pera_function.js
@@ -0,0 +1,21 @@
+/**
+ * multiple times to write function
+ */
+
+function add(a,b){
+    console.log(a + b);
+}
+function div(a,b){
+    console.log(a - b);
+}
+function multi(a,b) {
+    console.log(a * b);
+}
+function modulas(a,b){
+    console.log(a/b);
+}
+
+add(10,20);
+div(10,20);
+multi(10,20);
+modulas(10,20);
