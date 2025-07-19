cadcb292b594ff1174116df5c73e37c245fce3aa
Anamul Haque Anam <anamkhanbd2@gmail.com>
2025-07-19 15:43:48 +0600

    updated

--- this&arrowfunctions.js
+++ this&arrowfunctions.js
@@ -9,29 +9,45 @@
 
 
 
-let Persons = {
-    people: [
-        {
-            name : 'A H ANAM',
-            age  : 25,
-            interest : ['Gaming','Movie','Tour'],
-        },
-        {
-            name : 'Jiahd Sarker',
-            age  : 24,
-            interest : ['Football','Gaming','Eating']
-        },
-        {
-            name : 'Mithu Mia',
-            age  : 26,
-            interest : ['photography','videography','content Creator']
-        }
-    ],
-    show: function () {
-        this.people.forEach(el => {
+// let Persons = {
+//     people: [
+//         {
+//             name : 'A H ANAM',
+//             age  : 25,
+//             interest : ['Gaming','Movie','Tour'],
+//         },
+//         {
+//             name : 'Jiahd Sarker',
+//             age  : 24,
+//             interest : ['Football','Gaming','Eating']
+//         },
+//         {
+//             name : 'Mithu Mia',
+//             age  : 26,
+//             interest : ['photography','videography','content Creator']
+//         }
+//     ],
+//     show: function () {
+//         this.people.forEach(el => {
+//             console.log(el);
+//         });
+//     }
+// };
+
+// Persons.show(); // ✅ এখন ঠিকভাবে কাজ করবে
+
+
+// make a object....
+let Interest = {
+    anam : ['Gaming','Tour','Eating'],
+    mithu : ['photography','video graphy','content making'],
+    jihad : ['Tour','farming','gardening'],
+
+    showName : function(){
+        this.anam.forEach(el => {
             console.log(el);
         });
     }
-};
+}
 
-Persons.show(); // ✅ এখন ঠিকভাবে কাজ করবে
+Interest.showName();
